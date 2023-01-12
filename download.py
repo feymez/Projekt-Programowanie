8dd31ece5d3682cfe2263b46e04a1e78e29e03e7
feymez <braajanskiba123@gmail.com>
2023-01-12 22:01:18 +0100

    upload

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
-import os 
+import os, requests
+import urllib.request
 
 files = ["AS VAL", "OP-SKS", "SKS", "Vepr Hunter VPO-101", "AXMC", "DVL-10", "M700", "Mosin (Infantry)", "Mosin (Sniper)", "SV-98", "T-5000", "VPO-215", 
 "ADAR 2-15", "AK-74", "AK-74M", "AK-74N", "AK-101", "AK-102", "AK-103", "AK-104", "AK-105", "AKM", "AKMN", "AKMS", "AKMSN", "AKS-74", "AKS-74N", "AKS-74U", "AKS-74UB", "AKS-74UN", "ASh-12", "AUG A1", "AUG A3", "DT MDR 5.56x45", "DT MDR 7.62x51", "HK 416A5", "HK G36", "M4A1", "MCX", "Mk47", "RD-704", "SA-58", "SAG AK Short", "SAG AK", "SCAR-H", "SCAR-L", "TX-15 DML", "Vepr AKM VPO-209", "Vepr KM VPO-136"
@@ -11,3 +12,30 @@ files = ["AS VAL", "OP-SKS", "SKS", "Vepr Hunter VPO-101", "AXMC", "DVL-10", "M7
 "AGS-30", "NSV 'Utyos'",
 "KS-23M", "M3 Super 90", "M590A1", "M870", "MP-18", "MP-43-1C", "MP-133", "MP-153", "MP-155", "MTs-255-12", "Saiga-12", "TOZ-106",
 "GL40", "M32A1"]
+
+
+def check_connect():
+    try:
+        urllib.request.urlopen('http://google.com/')
+        return True
+    except:
+        return False
+
+def restart():
+    while True:
+        os.system('python main.py')
+        quit()
+
+for x in files:
+    file = os.path.exists(x)
+    if file == False:
+        print(f'Wykryto brakujący plik {x}.json')
+        internet_connection = check_connect()
+        if internet_connection == False:
+            print("Komputer nie ma połączenia z internetem.")
+            quit()
+        url = f'https://raw.githubusercontent.com/feymez/Projekt-Programowanie/main/bronie/karabinki-szturmowe/{x}.json'
+        response = requests.get(url)
+        print('Pobieram brakujący plik')
+        open(f'{x}.json', 'wb').write(response.content)
+        #restart()
