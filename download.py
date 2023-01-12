1fc2aaef96ce03329567d9f4752f06fee0052ae8
feymez <braajanskiba123@gmail.com>
2023-01-12 21:41:25 +0100

    upload

---
+++ download.py
@@ -0,0 +1,13 @@
+import os 
+
+files = ["AS VAL", "OP-SKS", "SKS", "Vepr Hunter VPO-101", "AXMC", "DVL-10", "M700", "Mosin (Infantry)", "Mosin (Sniper)", "SV-98", "T-5000", "VPO-215", 
+"ADAR 2-15", "AK-74", "AK-74M", "AK-74N", "AK-101", "AK-102", "AK-103", "AK-104", "AK-105", "AKM", "AKMN", "AKMS", "AKMSN", "AKS-74", "AKS-74N", "AKS-74U", "AKS-74UB", "AKS-74UN", "ASh-12", "AUG A1", "AUG A3", "DT MDR 5.56x45", "DT MDR 7.62x51", "HK 416A5", "HK G36", "M4A1", "MCX", "Mk47", "RD-704", "SA-58", "SAG AK Short", "SAG AK", "SCAR-H", "SCAR-L", "TX-15 DML", "Vepr AKM VPO-209", "Vepr KM VPO-136"
+"HK G28", "M1A", "Mk-18", "RFB", "RSASS", "SR-25", "SVDS", "VSS Vintorez"
+"RPK-15",
+"APB", "APS", "FN 5-7", "Glock 17", "Glock 18C", "Glock 19X", "M9A3", "M45A1", "M1911A1", "MP-443 _Grach_", "P226R", "PB pistol", "PL-15", "PM (t) pistol", "PM pistol", "SR-1MP Gyurza", "TT pistol (gold)", "TT pistol", "USP .45"
+"MP5", "MP5K", "MP7A1", "MP7A2", "MP9-N", "MP9", "MPX", "P90", "PP-9_Klin_", "PP-19-01 Vityaz-SN", "PP-91 _Kedr_", "PP-91-01 _Kedr-B_", "PPSh-41", "Saiga-9", "SR-2M", "STM-9", "UMP 45", "Vector .45", "Vector 9x19",
+"CR50DS", "CR200DS", "RSh-12",
+"SP-81",
+"AGS-30", "NSV 'Utyos'",
+"KS-23M", "M3 Super 90", "M590A1", "M870", "MP-18", "MP-43-1C", "MP-133", "MP-153", "MP-155", "MTs-255-12", "Saiga-12", "TOZ-106",
+"GL40", "M32A1"]
